137f2cea20c502b603da98b7a0560504c2944aa5
Thiago Gomes Parzanini <parzanini@hotmail.com>
2024-10-16 22:19:33 +0100

    fix routes pt and es

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,12 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import axios from 'axios'
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+import axios from 'axios';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/:lang(pt|es)?', // Only allow 'pt' and 'es' as language parameters, '/' for English
+      path: '/',
       name: 'home',
       component: HomeView,
       beforeEnter: async (to, from, next) => {
@@ -15,20 +15,18 @@ const router = createRouter({
 
         if (!lang || !supportedLanguages.includes(lang)) {
           try {
-            if (!lang) {
-              // Use a geolocation API to get the user's country
-              const response = await axios.get('https://ipapi.co/json/');
-              const country = response.data.country_code;
+            // Use a geolocation API to get the user's country
+            const response = await axios.get('https://ipapi.co/json/');
+            const country = response.data.country_code;
 
-              // Map countries to languages
-              const countryLanguageMap = {
-                BR: 'pt', PT: 'pt', AO: 'pt', MZ: 'pt',
-                ES: 'es', MX: 'es', AR: 'es', CO: 'es',
-                // Add more countries as needed
-              };
+            // Map countries to languages
+            const countryLanguageMap = {
+              BR: 'pt', PT: 'pt', AO: 'pt', MZ: 'pt',
+              ES: 'es', MX: 'es', AR: 'es', CO: 'es',
+              // Add more countries as needed
+            };
 
-              lang = countryLanguageMap[country];
-            }
+            lang = countryLanguageMap[country];
 
             if (lang && supportedLanguages.includes(lang)) {
               next(`/${lang}`);
@@ -43,8 +41,13 @@ const router = createRouter({
           next(); // Proceed with the navigation if the language is already set and supported
         }
       }
+    },
+    {
+      path: '/:lang(pt|es)',
+      name: 'home-lang',
+      component: HomeView,
     }
   ]
-})
+});
 
-export default router
+export default router;
